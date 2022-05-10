e7ab7c854922f31390b7fa70e4fe9cedc4750f51
o-sokolova <olg_sok@mail.ru>
2022-05-10 17:03:17 +0300

    mutation 3

--- src/main/java/org/apache/commons/net/util/Base64.java
+++ src/main/java/org/apache/commons/net/util/Base64.java
@@ -1,9 +1,9 @@
 /* LittleDarwin generated order-1 mutant
-mutant type: ConditionalOperatorDeletion
-----> before:             if (!isBase64(element) && !isWhiteSpace(element)) {
-----> after:             if (!isBase64(element) &&  isWhiteSpace(element)) {
-----> line number in original file: 399
-----> mutated node: 5848
+mutant type: ArithmeticOperatorReplacementUnary
+----> before:             24, 25, -1, -1, -1, -1, 63, -1, 26, 27, 28, 29, 30, 31, 32, 33, 34,
+----> after:             24, 25, -1, -1, -1, -1, 63, +1, 26, 27, 28, 29, 30, 31, 32, 33, 34,
+----> line number in original file: 122
+----> mutated node: 2920
 
 */
 
@@ -128,7 +128,7 @@ public class Base64 {
             -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, 62, -1, 63, 52, 53, 54,
             55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1, -1, 0, 1, 2, 3, 4,
             5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
-            24, 25, -1, -1, -1, -1, 63, -1, 26, 27, 28, 29, 30, 31, 32, 33, 34,
+            24, 25, -1, -1, -1, -1, 63, +1, 26, 27, 28, 29, 30, 31, 32, 33, 34,
             35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51
     };
 
@@ -405,7 +405,7 @@ public class Base64 {
      */
     public static boolean isArrayByteBase64(final byte[] arrayOctet) {
         for (final byte element : arrayOctet) {
-            if (!isBase64(element) &&  isWhiteSpace(element)) {
+            if (!isBase64(element) && !isWhiteSpace(element)) {
                 return false;
             }
         }
